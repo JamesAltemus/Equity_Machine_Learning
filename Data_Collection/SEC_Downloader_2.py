64b61d5d6cfcca323e110f39ec16d7fa507b16d3
James Altemus <61328750+JamesAltemus@users.noreply.github.com>
2020-02-27 13:03:08 -0600

    Update SEC_Downloader_2.py

--- Data_Collection/SEC_Downloader_2.py
+++ Data_Collection/SEC_Downloader_2.py
@@ -7,16 +7,16 @@ Created on Mon Jan 20 14:18:20 2020
 
 import os
 import csv
+
+import SEC_Archive_Retriever as arc
 import SEC_Downloader_Classes as sec
 
 from time import sleep
 
-store_in = ''
-index_archive = ''
-num_parallel = 6
+num_parallel = 1
 filings = ['10-K','10-Q','8-K','S-1','DRS','10-K/A','10-Q/A','8-K/A','S-1/A','DRS/A']
 
-def download(tiks, ciks, p, indicies, types):
+def download(tiks, ciks, p, types):
     if p == -1 or len(tiks) == 0:
         print ()
     else:
@@ -43,18 +43,17 @@ def download(tiks, ciks, p, indicies, types):
         print('Ticker:', tik, ', CIK:', cik, ', Number Remaining:', p)
         
         try:
-            sec.SEC_Filings(cik, alt_name=tik, archive = indicies, form_types = types)
-            os.chdir(d)
+            sec.SEC_Filings(cik, alt_name=tik, form_types = types)
             with open('sucess.txt', 'a+') as failed:
                 failed.write(tik+','+cik+'\n')
         except Exception as e:
             os.chdir(d)
-            print(e)
+            print('\n'+e)
             with open('failed.txt', 'a+') as failed:
                 failed.write(tik+','+cik+','+str(e)+'\n')
             print('Error infomration saved to failed.txt.\n')
         sleep(2)
-        download(tiks, ciks, p-1, indicies, types)
+        download(tiks, ciks, p-1, types)
         print (tik + ":" + cik)
 
 
@@ -70,5 +69,9 @@ with open('company_info.csv', 'r') as file:
         tiks.append(line[0])
         ciks.append(line[1])
 
-os.chdir(store_in)
-download(tiks, ciks, len(tiks)/num_parallel, index_archive, filings)
+if not os.path.exists('SEC_Filings'):
+    os.mkdir('SEC_Filings')
+os.chdir('SEC_Filings')
+
+arc.build_archive()
+download(tiks, ciks, len(tiks)/num_parallel, filings)
