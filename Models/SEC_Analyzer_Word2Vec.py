9a854c7e3fde8ee735dedf875041023d1297e14f
James Altemus <61328750+JamesAltemus@users.noreply.github.com>
2020-03-09 16:04:40 -0500

    Update SEC_Analyzer_Word2Vec.py

--- Models/SEC_Analyzer_Word2Vec.py
+++ Models/SEC_Analyzer_Word2Vec.py
@@ -9,24 +9,51 @@ import os
 
 from time import time, sleep
 from gensim.models import Word2Vec
-from SEC_Analyzer_Backend import filing_loader, progress, predict_time
+from nltk.tokenize import word_tokenize
+from SEC_Analyzer_Backend import progress, predict_time
 
-save_dir = 'Directory to store the saved model in'
-sec_dir = 'directory where SEC filings are'
 saver = 'SEC_w2v.model'
 
+def filing_loader(file_name, min_length = 3):
+    '''
+    file_name: the name of the txt file to load
+    dictionary: default None. If dictonary evaluates to true then it embeds the words in the document by the dictionary
+    embed_len: default 128. The length of the 'definition' in the dictionary if the word is not found in the dictionary
+    force_length: default False. Accepts false or a tuple of (sentence length, document length)
+    '''
+    doc = []
+    word_count = 0
+    with open(file_name) as text:
+        for line in text:
+            if len(line) < min_length:
+                continue
+            
+            line = line.replace('\n','')
+            line = word_tokenize(line)
+            while '.' in line:
+                ed = line.index('.')
+                new = line[0:ed]
+                word_count += len(new)
+                doc.append(new)
+                line = line[ed+1:]
+            word_count += len(line)
+            doc.append(line)
+    sent_count = len(doc)
+    return doc, word_count, sent_count
+
+
 # Create sample for testing
-os.chdir(sec_dir)
+os.chdir('SEC_Filings')
 companies = os.listdir()
 encoding_sample_size = len(companies)
-os.chdir(save_dir)
+os.chdir('../')
 
 times = []
 done = 0
 stopper = 0
 for company in companies:
     bg = time()
-    os.chdir(os.path.join(sec_dir, company))
+    os.chdir(os.path.join('SEC_Filings', company))
     print('Analyzing {}...'.format(company))
     filings = os.listdir()
     filings.remove('Tables')
@@ -46,7 +73,8 @@ for company in companies:
         progress(done2, complete)
     
     # Save checkpoint
-    os.chdir(save_dir)
+    os.chdir('../')
+    os.chdir('../')
     model.save(saver)
     model.wv.save('vocabulary.kv')
     
